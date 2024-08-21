3bc720cbcecdad6d2ce10d0f94692b67ffbdca20
Saqib <splasher733@gmail.com>
2024-08-21 14:09:53 +0500

    Removed uneccesary warnings

--- test.py
+++ test.py
@@ -1,43 +1,44 @@
+import os
 import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import matplotlib.pyplot as plt
+import tensorflow as tf
+import warnings
+
+# Suppress TensorFlow warnings
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+tf.get_logger().setLevel('ERROR')
+
+# Suppress all warnings
+warnings.filterwarnings('ignore')
 
 # Load the pre-trained discriminator model
 discriminator = load_model('discriminator.h5')
 
-def preprocess_image(img_path):
+def classify_image(img_path, model):
     # Load and preprocess the image
     img = image.load_img(img_path, target_size=(32, 32))  # Resize to (32, 32) as expected by the model
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
     
-    # Normalize the image to [-1, 1], same as during training
-    img_array = (img_array - 127.5) / 127.5
-    
-    return img_array
-
-def classify_image(img_path, model):
-    img_array = preprocess_image(img_path)
+    # If preprocessing was applied during training, include it here
+    img_array = img_array / 255.0  # Uncomment if normalization was used
 
     # Predict
     prediction = model.predict(img_array)
-    print(f"Discriminator raw output: {prediction}")
 
     # Interpret the result
     threshold = 0.5
     if prediction[0][0] > threshold:
-        return "Real"
+        return "\033[1mReal\033[0m"
     else:
-        return "Fake"
+        return "\033[1mFake\033[0m"
 
 # Path to the image you want to test
-img_path = 'ganPanda.png'
+img_path = 'putin.jpeg'
 result = classify_image(img_path, discriminator)
 print(f"The image is: {result}")
 
-# Display the image
-img = image.load_img(img_path, target_size=(32, 32))  # Resize to (32, 32) for display
-plt.imshow(img)
-plt.title(f"Classified as: {result}")
-plt.show()
+# Disable Matplotlib warning for non-GUI backend
+plt.switch_backend('agg')
